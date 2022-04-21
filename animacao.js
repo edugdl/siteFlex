e0e38a929123be0f9bcc3c4835d6921deb243ffc
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-21 20:35:14 -0300

    logo

--- animacao.js
+++ animacao.js
@@ -1,3 +1,7 @@
-if( $(window).width() < 700){
-    document.getElementById("imagem").style.paddingLeft = 0;
-}
+$(document).ready(function(){
+    if( $(window).width() > 700){
+        document.getElementById("imagem").style.paddingLeft = "10%";
+    }else{
+        document.getElementById("imagem").style.paddingLeft = "0";
+    }
+});
