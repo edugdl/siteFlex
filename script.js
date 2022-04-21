b72722ef20aca0da66d29f87e7a4e5a3f4b3a6e0
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-21 15:07:24 -0300

    header-responsiva

--- script.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    $(window).resize(function(){
-        var winW = $(window).width();
-        var divResize = $(".resize");
-        if(winW < 500)
-            divResize.css('background-color', 'green');
-    });
-});
