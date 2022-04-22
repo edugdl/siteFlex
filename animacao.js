ff2200dc6f7465e8ef74dce3f6e4aad759d3ca17
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-22 16:10:33 -0300

    desktop&&mobile

--- animacao.js
+++ animacao.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-    if(window.screen.width <= 800 
-        && !window.location.href.includes("index-mo.html")){
-        window.location.href = 'index-mo.html';
-    }else if(window.screen.width > 800 
-        && !window.location.href.includes("index.html")){
-        window.location.href = 'index.html';
-    }
+    let navCount = 0;
     $('#botao-sideBar').click(function(){
         let nav = document.querySelector("#nav-sideBar");
         nav.classList.toggle("aparecer");
-        nav.classList.toggle("desaparecer");
+        if(navCount>0){
+            nav.classList.toggle("desaparecer");
+        }else if(navCount==0){
+            nav.classList.toggle("pre-aparecer");
+        }
         navCount++;
     });
 });
