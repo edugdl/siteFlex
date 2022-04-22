30cd0137ac8b7f39be4a89edd9f0bef07ba67664
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-22 03:56:28 -0300

    sideBar

--- animacao.js
+++ animacao.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    if(window.screen.width <= 900) {
-        window.location.href = 'https://www.google.com.br';
+    if(window.screen.width <= 800 
+        && !window.location.href.includes("index-mo.html")){
+        window.location.href = 'index-mo.html';
+    }else if(window.screen.width > 800 
+        && !window.location.href.includes("index.html")){
+        window.location.href = 'index.html';
     }
+    let sideBarN = 2;
+    $('#botao-sideBar').click(function(){
+        document.querySelector("#nav-sideBar").classList.toggle("aparecer");
+        if(sideBarN % 2 == 0){
+            document.getElementById("nav-sideBar").style.display = "flex";
+        }else{
+            document.getElementById("nav-sideBar").style.display = "none";
+        }
+        sideBarN++;
+    });
 });
