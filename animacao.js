d7184f1f62edb06971a6d8da88bd3ea32ce4b5ef
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-22 09:02:05 -0300

    Update animacao.js

--- animacao.js
+++ animacao.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    history.pushState(null, null, document.URL);
+    window.addEventListener('popstate', function () {
+        history.pushState(null, null, document.URL);
+    });
     if(window.screen.width <= 800 
         && !window.location.href.includes("index-mo.html")){
         window.location.href = 'index-mo.html';
