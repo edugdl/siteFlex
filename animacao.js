b3a48c621426b408a1ab3c552d323b49d15dd3ad
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-04-22 04:12:27 -0300

    fadeIn

--- animacao.js
+++ animacao.js
@@ -6,14 +6,7 @@ $(document).ready(function(){
         && !window.location.href.includes("index.html")){
         window.location.href = 'index.html';
     }
-    let sideBarN = 2;
     $('#botao-sideBar').click(function(){
         document.querySelector("#nav-sideBar").classList.toggle("aparecer");
-        if(sideBarN % 2 == 0){
-            document.getElementById("nav-sideBar").style.display = "flex";
-        }else{
-            document.getElementById("nav-sideBar").style.display = "none";
-        }
-        sideBarN++;
     });
 });
